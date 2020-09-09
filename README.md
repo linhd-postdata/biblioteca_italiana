fabbb91d75b2d2cf0b4eefe4bbb94a38e8f62c7c
Aitor Diaz Medina <adiazm@scc.uned.es>
2020-09-09 16:45:24 +0200

    Added JSON version of the corpus

--- README.md
+++ README.md
@@ -1,13 +1,47 @@
 # biblioteca_italiana
 This corpus is a subset in XML TEI format of the available poems in the poetry corpus from the Biblioteca Italiana
 
-It containes more than 18000 works from over 159 authors
+It contains more than 18000 works from over 159 authors
 
 ## XML folder statistics
 
-- Authors: 159
+- Authors: 183
 - Works: 18689
-- Stanzas: 100892
 - Verses: 429969
 - Words: 2797182
 - Characters: 14910102
+
+The file [`biblitaliana.zip`](./biblitaliana.zip) contains the compressed JSON corpus. The format of each entry is as follows:
+```json
+{
+    "url": "https://github.com/linhd-postdata/biblioteca_italiana/blob/master/xml/bibit000213",
+    "author": "Dante Alighieri",
+    "collection": "Il Fiore",
+    "title": "I",
+    "manually_checked": false,
+    "text": [
+        [
+            {
+                "verse": "Lo Dio d'Amor con su' arco mi trasse",
+                "words": [
+                    "Lo",
+                    "Dio",
+                    "d'Amor",
+                    "con",
+                    "su'",
+                    "arco",
+                    "mi",
+                    "trasse"
+                ]
+            },
+            ...
+        ],
+        ...
+    ]
+},
+...
+```
+
+Folder [`json`](./json) contains the works by author, and [`xml`](./xml) contains the XML TEI version of the text.
+
+The script [`biblioteca_italiana.py`](./biblioteca_italiana.py) was used to build the json files.
---
+++ biblioteca_italiana.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# !pip install lxml python-slugify
+from lxml import etree
+import json
+from pathlib import Path
+from slugify import slugify
+
+
+def main():
+    authors_works = {}
+    work_list = []
+    line_counter = 0
+    word_counter = 0
+    char_counter = 0
+    for file in Path("./xml").glob('*'):
+        xml_file = str(file)
+        try:
+            tree = etree.parse(xml_file)
+        except etree.ParseError as e:
+            pass
+        else:
+            root = tree.getroot()
+            manually_checked = False
+            collection_title = root.find(".//title").text
+            author = root.find(f".//author")
+            author_text = "unknown"
+            if author is not None:
+                author_text = author.text
+            poem_list = root.findall(f".//div1[@type='poesia']")
+            poem_list2 = root.findall(f".//div2[@type='poesia']")
+            poem_list.extend(poem_list2)
+            for poem_number, poem in enumerate(poem_list):
+                poem_dict = {}
+                if author_text == "unknown":
+                    poem_parent_head = poem.getparent().find("head")
+                    if poem_parent_head is not None:
+                        author_text = poem_parent_head.text
+                        # all poems with 'unknown' author are from bibit000818 file
+                if author_text not in authors_works:
+                    authors_works[author_text] = []
+                poem_head = poem.find("head")
+                poem_title = poem_number
+                if poem_head is not None:
+                    if poem_head.text is not None:
+                        poem_title = poem_head.text
+                #poem_type = poem.find(f"lg").attrib["type"]
+                stanzas = []
+                poem_lg = poem.find(f"lg")
+                if poem_lg is not None:
+                    for stanza in poem_lg.findall("lg"):
+                        lines = []
+                        for l in stanza.findall("l"):
+                            if l.text is not None:
+                                text = l.text
+                                lines.append({
+                                    "verse": text,
+                                    "words": text.split()
+                                })
+                                line_counter += 1
+                                word_counter += len(text.split())
+                                char_counter += len(text)
+                        if len(lines)>0:
+                            stanzas.append(lines)
+                    if len(stanzas)>0:
+                        authors_works[author_text].append({
+                            "url": f"https://github.com/linhd-postdata/biblioteca_italiana/blob/master/{xml_file}",
+                            "author": author_text,
+                            "collection": collection_title,
+                            "title": poem_title,
+                            "manually_checked": manually_checked,
+                            "text": stanzas,
+                        })
+                        work_list.append(f"{author_text} - {collection_title} - {poem_title}")
+    all_works = []
+    for author_name, author_works in authors_works.items():
+        author_slug = slugify(author_name, separator="_")
+        with open(Path("json") / f"{author_slug}.json", "w", encoding='utf-8') as author_json:
+            json.dump(author_works, author_json, ensure_ascii=False, indent=4)
+            all_works += author_works
+    with open("biblitaliana.json", "w", encoding='utf-8') as all_authors:
+        json.dump(all_works, all_authors, ensure_ascii=False, indent=4)
+    print("Statistics\n----------")
+    print("- Authors:", len(authors_works))
+    print("- Works:", len(work_list))
+    print("- Verses:", line_counter)
+    print("- Words:", word_counter)
+    print("- Characters:", char_counter)
+
+
+if __name__ == "__main__":
+    main()
